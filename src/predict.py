8bbbf53e45a79bc229ad19291997e76afe3c65c1
shaySirek <shaysirek@gmail.com>
2021-06-23 16:30:19 +0300

    add docs
    Signed-off-by: shaySirek <shaysirek@gmail.com>

--- src/predict.py
+++ src/predict.py
@@ -41,6 +41,34 @@ def _create_avocado_data_frame(avocado: dict) -> pd.DataFrame:
 
 
 def predict(avocado: dict) -> float:
+    """
+    Parameters
+    ----------
+    avocado : dict
+        avocado instance data
+    
+    Returns
+    -------
+    float
+        if avocado is valid, its predicted price is returned.
+        otherwise , 0 is returned.
+
+    Example
+    -------
+    avocado = {
+        'sold_plu_4046': 5,
+        'sold_plu_4225': 6,
+        'sold_plu_4770': 8,
+        'small_bags': 4,
+        'large_bags': 0,
+        'xlarge_bags': 0,
+        'organic': True,
+        'region': 'Albany',
+        'date': '2020-12-27'
+    }
+    price = predict(avocado)
+    """
+
     x = _create_avocado_data_frame(avocado)
     y = 0.0
     if x:
